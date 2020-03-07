32b76e2d19abbe50fecb5f3b211aeabc570c4d3a
Nick Boober <nboober@terpmail.umd.edu>
2020-03-07 13:45:03 -0500

    successfully fetched hangman word from backend

--- src/App.js
+++ src/App.js
@@ -2,6 +2,29 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component{
+  constructor(){
+    super()
+    this.state={
+      word: ""
+    }
+  }
+
+  componentDidMount = () => {
+
+    this.fetchNewWord()
+  
+  }
+  
+  fetchNewWord = () => {
+    fetch("http://localhost:3000/word")
+    .then(response => response.json())
+    .then(wordArray => {
+      this.setState({
+        word: wordArray[0]
+      })
+    })
+  }
+
   render(){
     return(
       <div>
