b88eb745c155c13561805581b9d55b24e4d2c1a0
Nick Boober <nboober@terpmail.umd.edu>
2020-03-07 16:12:12 -0500

    created a function in app.js that randomizes and adds new letters to the list of letters to choose from in state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import GuessWord from './components/guessWord'
 import Letters from './components/letters'
 import Man from './components/man'
 import Word from './components/word'
+import _ from 'underscore'
 import './App.css';
 
 class App extends React.Component{
@@ -10,7 +11,8 @@ class App extends React.Component{
     super()
     this.state={
       word: "",
-      splitWord: ""
+      splitWord: "",
+      randomizedLetters: ""
     }
   }
 
@@ -27,8 +29,43 @@ class App extends React.Component{
       this.setState({
         word: wordArray[0],
         splitWord: wordArray[0].split("")
+      },()=>{
+        this.randomizedLetters()
       })
     })
+  }
+
+  shuffle = (array) => {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+
+  randomizedLetters = () => {
+    let newArray = [...this.state.splitWord]
+
+    let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+    let getRandomLetter = function(){ newArray.push(alphabet[Math.floor(Math.random() * (alphabet.length - 1)) ]) }
+
+    _.times(20, getRandomLetter )
+
+    // console.log(this.shuffle(_.uniq(newArray)))
+    this.setState({randomizedLetters: this.shuffle(_.uniq(newArray))})
+
   }
 
   guessWord = (event) => {
@@ -39,7 +76,7 @@ class App extends React.Component{
     return(
       <div>
         <GuessWord guessWord={this.guessWord} />
-        <Letters/>
+        <Letters letters={this.state.randomizedLetters} />
         <Man/>
         <Word word={this.state.word} split={this.state.splitWord}/>
       </div>
