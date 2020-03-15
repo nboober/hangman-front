a03125c5133f9035e8b95bc4a2b776f807bfe1c5
Nick Boober <nboober@terpmail.umd.edu>
2020-03-15 13:17:48 -0400

    the high scores component now shows all the users and their high scores

--- src/components/highscores.js
+++ src/components/highscores.js
@@ -1,10 +1,32 @@
 import React from 'react'
 
 class HighScores extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            users: ""
+                }
+    }
+
+    componentDidMount = () => {
+        fetch('http://localhost:3000/allplayers')
+        .then(respsonse => respsonse.json())
+        .then(usersArray => {
+            this.setState({
+                users: usersArray
+            })
+        })
+    }
+
     render(){
         return(
             <div>
-                Users
+                <h2>High Scores</h2>
+                {this.state.users ? 
+                    this.state.users.map((user)=>{
+                        return <div>{user.initials} : Level {user.level}</div>
+                    })
+                : null}
             </div>
         )
     }
